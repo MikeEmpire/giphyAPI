081d33ae5c00aede7e579fc6ead6ad98aad21e6d
Mike <aolie94@gmail.com>
2017-03-16 22:49:22 -0700

    Created on click functionality for when gifs reload

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,10 +40,24 @@ function showGifs() {
                 var playerImg = $("<img>");
                 // Setting the src attribute of the image to a property pulled off the result item
                 playerImg.attr("src", results[i].images.fixed_height.url);
+                playerImg.addClass("gif");
+                playerImg.attr("data-animate", results[i].images.fixed_height.url);
+                playerImg.attr("data-still", results[i].images.fixed_height.url);
+                playerImg.attr("data-state", "still");
                 playerDiv.append(p);
                 playerDiv.append(playerImg);
             }
-        })
+        });
 }
 $(document).on("click", ".player", showGifs);
+$(document).on("click", ".gif", function() {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
 generateButtons();
